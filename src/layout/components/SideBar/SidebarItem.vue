3c4b7f769331d8f1626e33b22dd01e917bfe2d17
386083520 <386083520@qq.com>
2022-05-12 10:15:11 +0800

    引入SidebarItem

---
+++ src/layout/components/SideBar/SidebarItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-submenu index="1">
+    <template slot="title">
+      <i class="el-icon-location"></i>
+      <span>导航一</span>
+    </template>
+    <el-menu-item index="1-4-1">选项14</el-menu-item>
+    <el-submenu index="1-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="1-4-1">选项1</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+</template>
+
+<script>
+export default {
+  name: 'SidebarItem'
+}
+</script>
+
+<style scoped>
+
+</style>
