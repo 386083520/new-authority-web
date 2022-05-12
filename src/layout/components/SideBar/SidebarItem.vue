4d9a485d362d6c48f54f48eac1f0ebbed5860016
386083520 <386083520@qq.com>
2022-05-12 10:26:09 +0800

    了解getRouters的数据结构

--- src/layout/components/SideBar/SidebarItem.vue
+++ src/layout/components/SideBar/SidebarItem.vue
@@ -1,20 +1,16 @@
 <template>
-  <el-submenu index="1">
-    <template slot="title">
-      <i class="el-icon-location"></i>
-      <span>导航一</span>
-    </template>
-    <el-menu-item index="1-4-1">选项14</el-menu-item>
-    <el-submenu index="1-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="1-4-1">选项1</el-menu-item>
-    </el-submenu>
-  </el-submenu>
+  <el-menu-item index="1-4-1">{{item.name}}</el-menu-item>
 </template>
 
 <script>
 export default {
-  name: 'SidebarItem'
+  name: 'SidebarItem',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
 
