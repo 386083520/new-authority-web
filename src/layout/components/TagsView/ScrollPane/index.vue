5f03880474453071d6e5fb18fad8cbcaa62db0db
386083520 <386083520@qq.com>
2022-05-17 17:00:44 +0800

    tagsview样式调整

---
+++ src/layout/components/TagsView/ScrollPane/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-scrollbar :vertical="false" class="scroll-container">
+      <slot></slot>
+    </el-scrollbar>
+</template>
+
+<script>
+export default {
+  name: 'ScrollPan'
+}
+</script>
+
+<style scoped lang="scss">
+.scroller-container {
+}
+</style>
--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -1,15 +1,72 @@
 <template>
-    <div>
-      TagsView
+    <div class="tags-view-container">
+      <scroll-pan class="tags-view-wrapper">
+        <router-link
+          v-for="tag in visitedViews"
+          :key="tag.path"
+          tag="span"
+          :to="{path: tag.path}"
+          class="tags-view-item"
+        >
+          {{tag.title}}
+        </router-link>
+      </scroll-pan>
     </div>
 </template>
 
 <script>
+import ScrollPan from './ScrollPane/index'
 export default {
-  name: 'TagsView'
+  name: 'TagsView',
+  components: { ScrollPan },
+  data () {
+    return {
+      visitedViews: [
+        {
+          title: '首页',
+          path: 'test'
+        },
+        {
+          title: 'test1',
+          path: 'test1'
+        },
+        {
+          title: 'test2',
+          path: 'test2'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.tags-view-container {
+  height: 34px;
+  width: 100%;
+  background: #fff;
+  border-bottom: 1px solid #d8dce5;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 12%), 0 0 3px 0 rgb(0 0 0 / 4%);
+  .tags-view-wrapper {
+    .tags-view-item {
+      display: inline-block;
+      height: 26px;
+      line-height: 26px;
+      border: 1px solid #d8dce5;
+      padding: 0 8px;
+      margin-left: 5px;
+      margin-top: 4px;
+      cursor: pointer;
+      color: #495060;
+      font-size: 12px;
+      background: #fff;
+      &:first-of-type {
+        margin-left: 15px;
+      }
+      &:last-of-type {
+        margin-right: 15px;
+      }
+    }
+  }
+}
 </style>
