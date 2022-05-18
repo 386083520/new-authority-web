98d0b5443562d3ac8ff26b91a91cdfcf9e31a2ad
386083520 <386083520@qq.com>
2022-05-18 15:08:49 +0800

    添加新的页面

---
+++ src/views/system/dept/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>dept</div>
+</template>
+
+<script>
+export default {
+  name: 'dept'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/system/menu/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>menu</div>
+</template>
+
+<script>
+export default {
+  name: 'menu'
+}
+</script>
+
+<style scoped>
+
+</style>
